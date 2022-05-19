8ff426408e42f68c1dce70c17f750262a0e59816
radiney <99511595+radiney@users.noreply.github.com>
2022-05-18 20:33:26 -0700

    Simple Camera app
    in class demo for using camera

--- simpleCam/ViewController.swift
+++ simpleCam/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnCapture: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        btnCapture.backgroundColor = .systemBlue
+        btnCapture.setTitle("Capture", for: .normal)
+        btnCapture.setTitleColor(.white, for: .normal)
+        
+        imageView.backgroundColor = .secondarySystemBackground
     }
 
+    @IBAction func btnCapture(_ sender: UIButton) {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+}
 
+extension ViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return
+        }
+        imageView.image = image
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
 
