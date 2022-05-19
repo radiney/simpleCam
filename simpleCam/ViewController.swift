bc37c59a06637ccb5d09d6310c8cb35f0a8b000a
radiney <99511595+radiney@users.noreply.github.com>
2022-05-18 19:31:55 -0700

    Initial Commit

---
+++ simpleCam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simpleCam
+//
+//  Created by user217360 on 5/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
